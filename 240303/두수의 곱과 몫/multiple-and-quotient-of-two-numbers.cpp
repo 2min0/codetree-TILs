0db36862963557cdf48becd9d12bb87ae978a093
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-03-03 18:28:10 +0900

    Title: 두수의 곱과 몫, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240303/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
+++ 240303/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> a;
     cin.get();
     cin >> b;
-    cout << a << " * " << b << " = " << a*b;
+    cout << a << " * " << b << " = " << a*b << endl;
     cout << a << " / " << b << " = " << a/b;
     return 0;
 }
