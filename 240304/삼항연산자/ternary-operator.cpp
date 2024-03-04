e9e04c1d04ec00243eddadbb90bc86f25bb1c38a
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-03-04 21:52:28 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240304/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    string ans = n == 100 ? "pass" : "failure";
+    cout << ans;
+    return 0;
+}
