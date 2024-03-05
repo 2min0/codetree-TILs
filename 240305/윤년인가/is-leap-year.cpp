7479cc0c02a72c41543c33b3b874f00b731592a8
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-03-05 10:13:47 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240305/윤년인가/is-leap-year.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    if (n % 4 == 0){
+        if ((n%100 == 0) && (n % 400 != 0)){
+            cout << false;
+        }
+        else{
+            cout << true;
+        }
+    }
+    return 0;
+}
