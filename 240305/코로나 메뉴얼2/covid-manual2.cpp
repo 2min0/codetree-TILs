8371e850ca5e3bf1eae348f412575140bf079fea
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-03-05 16:55:22 +0900

    Title: 코로나 메뉴얼2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240305/코로나 메뉴얼2/covid-manual2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[4] = {};
+    for (int i=0; i<3; i++){
+        char a;
+        int b;
+        cin >> a >> b;
+        if (a == 'Y' && b >= 37){
+            arr[0] += 1;
+        }
+        else if (a == 'N' && b >= 37){
+            arr[1] += 1;
+        }
+        else if (a == 'Y' && b <37){
+            arr[2] += 1;
+        }
+        else{
+            arr[3] += 1;
+        }
+    }
+    for (int i = 0; i<4; i++){
+        cout << arr[i] << " ";
+    }
+    if (arr[0] >= 2){
+        cout << 'E';
+    }
+    return 0;
+}
