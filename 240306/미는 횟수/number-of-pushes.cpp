1316b38ceae75fb0f5e34091c80ee9d1b7638c1b
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-03-06 14:43:19 +0900

    Title: 미는 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240306/미는 횟수/number-of-pushes.cpp
+++ 240306/미는 횟수/number-of-pushes.cpp
@@ -6,9 +6,16 @@ int main() {
     // 여기에 코드를 작성해주세요.
     string a, b;
     cin >> a >> b;
-    a = a + a;
-    if (a.find(b) != string::npos){
-        cout << a.find(b);
+    string aa = a + a;
+    if (aa.find(b) != string::npos){
+        int val = aa.find(b);
+        string bb = b+b;
+        if (bb.find(a) < val){
+            cout << bb.find(a);
+        }
+        else{
+            cout << val;
+        }
     }
     else{
         cout << -1;
