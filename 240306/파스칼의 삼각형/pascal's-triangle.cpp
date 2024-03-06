e0e5ab8008d36a29ead6ce0186218ff2dcc50f09
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-03-06 10:34:17 +0900

    Title: 파스칼의 삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/파스칼의 삼각형/pascal's-triangle.cpp
+++ 240306/파스칼의 삼각형/pascal's-triangle.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> n;
     int arr[n][n] = {};
     for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
+        for (int j=0; j<=i; j++){
             if (j==0 || i==j){
                 arr[i][j] = 1;
             }
