3daf4f2a2d93010da0029d0ba45df31089f3badc
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-03-06 14:24:36 +0900

    Title: 정수만 추출하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/정수만 추출하기/extract-only-integers.cpp
+++ 240306/정수만 추출하기/extract-only-integers.cpp
@@ -7,15 +7,29 @@ int main() {
     string x, y;
     cin >> x >> y;
     int x_int, y_int;
+    bool change = false;
+
     for (int i =0; i<x.length(); i++){
         if (isdigit(x[i])==false){
             x_int = stoi(x.substr(0, i));
+            change = true;
+            break;
         }
     }
+
+    if (change == false){
+        x_int = stoi(x);
+    }
+    change = false;
     for (int i =0; i<y.length(); i++){
         if (isdigit(y[i])==false){
             y_int = stoi(y.substr(0, i));
+            change = true;
+            break;
         }
+    }
+    if (change == false){
+        y_int = stoi(y);
     }
     cout << x_int + y_int;
     return 0;
