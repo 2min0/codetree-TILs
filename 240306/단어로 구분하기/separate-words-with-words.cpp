ce9476c1e434ea7b65d497123ee522b6ddaf50c5
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-03-06 11:05:55 +0900

    Title: 단어로 구분하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for (int i=1; i<=10; i++){
+        string x;
+        cin >> x;
+        cout << x << endl;
+    }
+    return 0;
+}
