7c96a4cc121cbf05b53cf80394aff6edc4b6e17e
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-03-06 14:45:06 +0900

    Title: 미는 횟수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/미는 횟수/number-of-pushes.cpp
+++ 240306/미는 횟수/number-of-pushes.cpp
@@ -6,17 +6,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     string a, b;
     cin >> a >> b;
-    string aa = a + a;
-    if (aa.find(b) != string::npos){
-        int val = aa.find(b);
-        string bb = b+b;
-        if (bb.find(a) < val){
-            cout << bb.find(a);
+    string bb = b+b;
+    if (bb.find(a) != string::npos){
+        cout << bb.find(a);
         }
-        else{
-            cout << val;
-        }
-    }
     else{
         cout << -1;
     }
