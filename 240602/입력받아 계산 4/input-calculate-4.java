f5ecf51d76d9505b194c6ca49645317c5ac014c6
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-06-02 18:45:53 +0900

    Title: 입력받아 계산 4, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240602/입력받아 계산 4/input-calculate-4.java
+++ 240602/입력받아 계산 4/input-calculate-4.java
@@ -3,7 +3,7 @@ import java.util.Scanner
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt;
+        int a = sc.nextInt();
         print(a*2);
     }
 }
