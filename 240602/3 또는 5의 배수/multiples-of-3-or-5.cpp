8d3d50b43d09392f87c69b89f7b4296351a4eb7b
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-06-02 17:08:48 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240602/3 또는 5의 배수/multiples-of-3-or-5.cpp
+++ 240602/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a;
+    cin >> a;
     if (a%3 == 0)
         cout << "YES" << endl;
     else
         cout << "NO" << endl;
     if (a%5 == 0)
         cout << "YES";
-    else cout << "NO";
+    else
+        cout << "NO";
     return 0;
 }
