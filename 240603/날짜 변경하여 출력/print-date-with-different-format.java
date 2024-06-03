f8b1548834be813632d21c9eb654a0b2943e99d6
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-06-03 13:36:10 +0900

    Title: 날짜 변경하여 출력, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240603/날짜 변경하여 출력/print-date-with-different-format.java
+++ 240603/날짜 변경하여 출력/print-date-with-different-format.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        sc.useDelimiter(".");
-        String y = sc.next();
-        String m = sc.next();
-        String d = sc.next();
-        System.out.printf("%s-%s-%s", m, d, y);
+        sc.useDelimiter("\\.");
+        int y = sc.nextInt();
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+        System.out.printf("%d-%d-%d", m, d, y);
     }
 }
