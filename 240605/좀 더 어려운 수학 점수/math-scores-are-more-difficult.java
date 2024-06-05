8ad002aaaa360f16009324aef35345d493326580
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-06-05 16:51:14 +0900

    Title: 좀 더 어려운 수학 점수, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240605/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int mathA = sc.nextInt();
+        int engA = sc.nextInt();
+        int mathB = sc.nextInt();
+        int engB = sc.nextInt();
+
+        if (mathA > mathB) System.out.print('A');
+        else if (mathB > mathA) System.out.print('B');
+        else{
+            if (engA > engB) System.out.print('A');
+            else if (engB > engA) System.out.print('B');
+        }
+    }
+}
