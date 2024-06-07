7cfce4fefccbedebb22a365f1230149bf65432f3
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-06-07 11:21:15 +0900

    Title: 1까지 나누기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240607/1까지 나누기/divide-by-1.java
+++ 240607/1까지 나누기/divide-by-1.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
         int cnt = 0;
 
-        for (int i = 1; i<=n; i++){
+        for (int i = 1; i<=5000; i++){
             n /= i;
             cnt++;
             if (n <= 1){
