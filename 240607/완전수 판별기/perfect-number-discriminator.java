ba388219fadfd0b0638fbfd332dc18fa8d8b9f73
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-06-07 10:43:05 +0900

    Title: 완전수 판별기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240607/완전수 판별기/perfect-number-discriminator.java
+++ 240607/완전수 판별기/perfect-number-discriminator.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
 
         int ans = 0;
-        for (int i = 1; i<= n; i++){
+        for (int i = 1; i< n; i++){
             if (n%i == 0){
                 ans += i;
             }
