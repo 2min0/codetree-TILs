8aedce8598807c2ec02611a5ace461ad65a84321
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-06-07 11:33:18 +0900

    Title: 3n + 1 수열, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240607/3n + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int cnt = 0;
+
+        while(true){
+            if (N==1){
+                System.out.print(cnt);
+                break;
+            }
+            if (N%2 == 0)
+                N /= 2;
+            else{
+                N = N*3+1;
+            }
+            cnt++;
+        }
+    }
+}
