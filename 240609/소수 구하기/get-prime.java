a243bf552f98702f5b66ef2d10a5f4d1719477c4
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-06-09 18:56:20 +0900

    Title: 소수 구하기, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240609/소수 구하기/get-prime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 2; i<=n; i++){
+            if (i <= 3){
+                System.out.printf("%d ", i);
+                continue;}
+            else if (i%2 == 0) continue;
+
+            boolean success = true;
+            for (int j = 3; j<=i; j++){
+                if (i%j == 0){
+                    success = false;
+                    break;
+                }
+            }
+
+            if (success) System.out.printf("%d ", i);
+        }
+
+    }
+}
