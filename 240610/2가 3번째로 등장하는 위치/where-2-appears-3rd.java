054897e9ec00a1c98f49dfef3c1b06a89bd09324
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-06-10 15:11:32 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240610/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for (int i = 0; i<n; i++){
+            int m = sc.nextInt();
+            if(m == 2) cnt++;
+
+            if (cnt==3){
+                System.out.print(i+1);
+                break;
+            }
+        }
+    }
+}
