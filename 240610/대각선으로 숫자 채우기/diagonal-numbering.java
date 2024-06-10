ccbdcb937a5c10bb410547f606671c59d04a2af5
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-06-10 17:33:14 +0900

    Title: 대각선으로 숫자 채우기, Time: 536ms, Memory: 21MB, Status: Passed - CodeTree

--- 240610/대각선으로 숫자 채우기/diagonal-numbering.java
+++ 240610/대각선으로 숫자 채우기/diagonal-numbering.java
@@ -7,22 +7,18 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[][] arr = new int[n][m];
-
-        int re;
-        arr[0][0] = 1;
-        for (int j = 1; j<m; j++){
-            arr[0][j] = arr[0][j-1] + j;
-        }
-
-        int keep = 0;
-        for (int j = 1; j<m; j++){
+        
+        int cnt = 0;
+        for (int j = 0; j<m; j++){
             int x = 0;
             int y = j;
+
+            arr[x][y] = ++cnt;
 
             while(true){
                 if (x+1 >= n || y-1 < 0) break;
                 arr[x+1][y-1] = arr[x][y] + 1;
-                keep = arr[x+1][y-1];
+                cnt = arr[x+1][y-1];
                 if (++x >= n || --y < 0) break;
             }
         }
@@ -31,12 +27,12 @@ public class Main {
             int x = i;
             int y = m-1;
 
-            arr[x][y] = ++keep;
+            arr[x][y] = ++cnt;
 
             while(true){
                 if (x+1 >= n || y-1 < 0) break;
                 arr[x+1][y-1] = arr[x][y] + 1;
-                keep = arr[x+1][y-1];
+                cnt = arr[x+1][y-1];
                 if (++x >= n || --y < 0) break;
             }
         }
