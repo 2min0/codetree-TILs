0280aa38985ab88dbe54aac0171c98c1d45bd470
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-06-10 10:59:37 +0900

    Title: 10개 입력받기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240610/10개 입력받기/receive-10-inputs.java
+++ 240610/10개 입력받기/receive-10-inputs.java
@@ -4,19 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int sum = 0;
+        int sum = 0, cnt = 0;
 
         for (int i = 0; i<10; i++){
             int n = sc.nextInt();
 
             if (n == 0){
-                System.out.printf("%d %.1f", sum, (double)sum/i);
                 break;
             }
 
             else{
                 sum += n;
+                cnt++;
             }
         }
+
+        System.out.printf("%d %.1f", sum, (double)sum/cnt);
     }
 }
