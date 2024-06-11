8796d8c83256249d10e561a1b1841a2edc76a089
Minyoung Lee <90391927+2min0@users.noreply.github.com>
2024-06-11 15:51:01 +0900

    Title: 문자열 돌리기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240611/문자열 돌리기/SPin-SPring.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int L = str.length();
+        System.out.println(str);
+
+        for (int i = 0; i<L; i++){
+            String tmp = "";
+            tmp = str.charAt(str.length()-1) + str.substring(0, str.length()-1);
+
+            System.out.println(tmp);
+            str = tmp;
+        }
+    }
+}
